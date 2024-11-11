4ee2c2933e03e059c52f93b15753f29fb1e32f15
calgreenex <carlos.alvarez@greenex.cl>
2024-11-11 09:15:38 -0300

    qr

--- resources/views/admin/asistencia/create.blade.php
+++ resources/views/admin/asistencia/create.blade.php
@@ -104,46 +104,47 @@
         }
         /** Ugly function to write the results to a table dynamically. */
         function printScanResultPretty(codeId, decodedText, decodedResult) {
-            // let resultSection = document.getElementById('scanned-result');
-            // let tableBodyId = "scanned-result-table-body";
-            // if (!document.getElementById(tableBodyId)) {
-            //     let table = document.createElement("table");
-            //     table.className = "styled-table";
-            //     table.style.width = "100%";
-            //     resultSection.appendChild(table);
-            //     let theader = document.createElement('thead');
-            //     let trow = document.createElement('tr');
-            //     let th1 = document.createElement('td');
-            //     th1.innerText = "Count";
-            //     let th2 = document.createElement('td');
-            //     th2.innerText = "Format";
-            //     let th3 = document.createElement('td');
-            //     th3.innerText = "Result";
-            //     trow.appendChild(th1);
-            //     trow.appendChild(th2);
-            //     trow.appendChild(th3);
-            //     theader.appendChild(trow);
-            //     table.appendChild(theader);
-            //     let tbody = document.createElement("tbody");
-            //     tbody.id = tableBodyId;
-            //     table.appendChild(tbody);
-            // }
-            // let tbody = document.getElementById(tableBodyId);
-            // let trow = document.createElement('tr');
-            // let td1 = document.createElement('td');
-            // td1.innerText = `${codeId}`;
-            // let td2 = document.createElement('td');
-
-            const urlParams = new URL(`${decodedResult.result.format.formatName}`).searchParams;
+            let resultSection = document.getElementById('scanned-result');
+            let tableBodyId = "scanned-result-table-body";
+            if (!document.getElementById(tableBodyId)) {
+                let table = document.createElement("table");
+                table.className = "styled-table";
+                table.style.width = "100%";
+                resultSection.appendChild(table);
+                let theader = document.createElement('thead');
+                let trow = document.createElement('tr');
+                let th1 = document.createElement('td');
+                th1.innerText = "Count";
+                let th2 = document.createElement('td');
+                th2.innerText = "Format";
+                let th3 = document.createElement('td');
+                th3.innerText = "Result";
+                trow.appendChild(th1);
+                trow.appendChild(th2);
+                trow.appendChild(th3);
+                theader.appendChild(trow);
+                table.appendChild(theader);
+                let tbody = document.createElement("tbody");
+                tbody.id = tableBodyId;
+                table.appendChild(tbody);
+            }
+            let tbody = document.getElementById(tableBodyId);
+            let trow = document.createElement('tr');
+            let td1 = document.createElement('td');
+            td1.innerText = `${codeId}`;
+            let td2 = document.createElement('td');
+            td2.innerText = `${decodedResult.result.format.formatName}`;
+            let td3 = document.createElement('td');
+            td3.innerText = `${decodedText}`;
+            trow.appendChild(td1);
+            trow.appendChild(td2);
+            trow.appendChild(td3);
+            tbody.appendChild(trow);
+            const urlParams = new URL(`${decodedText}`).searchParams;
+            console.log(urlParams);
+            // Obtener el valor del parámetro "RUN"
             const runValue = urlParams.get("RUN");
             console.log(runValue);
-            alert(runValue);
-            // let td3 = document.createElement('td');
-            // td3.innerText = `${decodedText}`;
-            // trow.appendChild(td1);
-            // trow.appendChild(td2);
-            // trow.appendChild(td3);
-            // tbody.appendChild(trow);
         }
         docReady(function() {
             hljs.initHighlightingOnLoad();
