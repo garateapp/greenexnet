491ec1f131839b3f4500783d3c86cb8a2ac30596
calgreenex <carlos.alvarez@greenex.cl>
2024-11-11 09:36:39 -0300

    qr

--- resources/views/admin/asistencia/create.blade.php
+++ resources/views/admin/asistencia/create.blade.php
@@ -104,48 +104,14 @@
         }
         /** Ugly function to write the results to a table dynamically. */
         function printScanResultPretty(codeId, decodedText, decodedResult) {
-            let resultSection = document.getElementById('scanned-result');
-            let tableBodyId = "scanned-result-table-body";
-            if (!document.getElementById(tableBodyId)) {
-                let table = document.createElement("table");
-                table.className = "styled-table";
-                table.style.width = "100%";
-                resultSection.appendChild(table);
-                let theader = document.createElement('thead');
-                let trow = document.createElement('tr');
-                let th1 = document.createElement('td');
-                th1.innerText = "Count";
-                let th2 = document.createElement('td');
-                th2.innerText = "Format";
-                let th3 = document.createElement('td');
-                th3.innerText = "Result";
-                trow.appendChild(th1);
-                trow.appendChild(th2);
-                trow.appendChild(th3);
-                theader.appendChild(trow);
-                table.appendChild(theader);
-                let tbody = document.createElement("tbody");
-                tbody.id = tableBodyId;
-                table.appendChild(tbody);
-            }
-            let tbody = document.getElementById(tableBodyId);
-            let trow = document.createElement('tr');
-            let td1 = document.createElement('td');
-            td1.innerText = `${codeId}`;
-            let td2 = document.createElement('td');
-            td2.innerText = `${decodedResult.result.format.formatName}`;
-            let td3 = document.createElement('td');
-            td3.innerText = `${decodedText}`;
-            trow.appendChild(td1);
-            trow.appendChild(td2);
-            trow.appendChild(td3);
-            tbody.appendChild(trow);
+
             const urlParams = new URL(`${decodedText}`).searchParams;
             console.log(urlParams);
             // Obtener el valor del parámetro "RUN"
             const runValue = urlParams.get("RUN");
             console.log(runValue);
             $("#personal_id").val(runValue).trigger('change');
+
         }
         docReady(function() {
             hljs.initHighlightingOnLoad();
