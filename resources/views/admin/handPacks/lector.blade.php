cb30bc5998543c908aa12cb8e8730692bf7f0eaa
calgreenex <carlos.alvarez@greenex.cl>
2025-03-07 14:51:56 -0300

    .

--- resources/views/admin/handPacks/lector.blade.php
+++ resources/views/admin/handPacks/lector.blade.php
@@ -7,20 +7,20 @@
 
         <div class="card-body">
 
-                <div class="form-group">
-                    <label class="required" for="rut">{{ trans('cruds.handPack.fields.rut') }}</label>
-                    <input class="form-control" type="text" name="qr" id="qr" value="" required>
+            <div class="form-group">
+                <label class="required" for="rut">{{ trans('cruds.handPack.fields.rut') }}</label>
+                <input class="form-control" type="text" name="qr" id="qr" value="" required>
 
-                </div>
-                <span id="result" class="center"></span>
+            </div>
+            <span id="result" class="center"></span>
 
             <div id="scanned-result2"></div>
-                <div id="scanned-result"></div>
-                <div class="form-group">
-                    <button class="btn btn-danger" type="button" id="btnGuardar" style="display:none">
-                        {{ trans('global.save') }}
-                    </button>
-                </div>
+            <div id="scanned-result"></div>
+            <div class="form-group">
+                <button class="btn btn-danger" type="button" id="btnGuardar" style="display:none">
+                    {{ trans('global.save') }}
+                </button>
+            </div>
 
         </div>
     </div>
@@ -35,17 +35,43 @@
 
         $(document).ready(function() {
             const params = new URLSearchParams(window.location.search);
+            let rut='';
+            let embalaje='';
+            let guid='';
+            // Convertir a un objeto URLSearchParams
+            let queryString = window.location.search.split('?')[1];
+            let params2 = new URLSearchParams(queryString);
 
-// Extraer los valores de cada parámetro
-const rut = params.get("rut");
-const embalaje = params.get("embalaje");
-const guid = params.get("guid");
+            // Obtener el valor del parámetro "qr"
+            let qrValue = params2.get("qr");
+
+            if (qrValue) {
+                // Dividir los valores usando el separador "|"
+                let qrData = qrValue.split("|");
+
+                // Asignar cada parte a una variable
+                rut = qrData[0]; // 10.353.155-1
+                embalaje = qrData[1]; // 6 Kgs
+                guid = qrData[2]; // d362fac1
+
+                // Imprimir en consola o usarlo como necesites
+                console.log("rut:", rut);
+                console.log("embalaje:", embalaje);
+                console.log("guid:", guid);
+            } else {
+                console.log("El parámetro 'qr' no está presente en la URL.");
+                rut = params.get("rut");
+                embalaje = params.get("embalaje");
+                guid = params.get("guid");
+            }
+            // Extraer los valores de cada parámetro
+
             $("#qr").focus();
-            $("#qr").val(rut+"]"+embalaje+"]"+guid);
+            $("#qr").val(rut + "]" + embalaje + "]" + guid);
             var qrCodeValue = $("#qr").val();
             processQrCode(qrCodeValue);
-               // Función para procesar el QR (manual o escaneado)
-               function processQrCode(qrCodeValue) {
+            // Función para procesar el QR (manual o escaneado)
+            function processQrCode(qrCodeValue) {
                 qrCodeValue = qrCodeValue.replace(/[\r\n]+/g, '').trim();
                 console.log("Código QR escaneado: ", qrCodeValue);
 
@@ -57,7 +83,7 @@ const guid = params.get("guid");
                             _token: "{{ csrf_token() }}",
                             qr: qrCodeValue
                         },
-                        success: function (response) {
+                        success: function(response) {
                             console.log(response);
                             if (response.success) {
                                 $("#scanned-result2")
@@ -72,11 +98,11 @@ const guid = params.get("guid");
                             }
                             // Limpiar y enfocar
                             $("#qr").val("");
-                            setTimeout(function () {
+                            setTimeout(function() {
                                 $("#qr").focus();
                             }, 100);
                         },
-                        error: function (xhr) {
+                        error: function(xhr) {
                             $("#scanned-result2")
                                 .html("Error al procesar el QR")
                                 .addClass("alert alert-danger");
@@ -87,10 +113,10 @@ const guid = params.get("guid");
                     });
                 }
             }
-            $("#qr").on("change", function () {
+            $("#qr").on("change", function() {
                 var qrCodeValue = $(this).val();
                 processQrCode(qrCodeValue);
             });
-    });
+        });
     </script>
 @endsection
