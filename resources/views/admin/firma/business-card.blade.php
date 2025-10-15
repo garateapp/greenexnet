c6395401e396b4fede4c7825f07d338959a2d56e
calgreenex <carlos.alvarez@greenex.cl>
2025-10-15 16:17:00 -0300

    .

--- resources/views/admin/firma/business-card.blade.php
+++ resources/views/admin/firma/business-card.blade.php
@@ -35,13 +35,13 @@
     $wave = secure_asset('img/ondaverde1.0.png');
 @endphp
 
-<table width="600" cellpadding="0" cellspacing="0" role="presentation"
-    style="width:600px;border-collapse:collapse;background-color:#ffffff;border-radius:12px;box-shadow:0 4px 14px rgba(0,0,0,0.08);font-family:'Cabin', Arial, Helvetica, sans-serif;color:#333333;">
+<table cellpadding="0" cellspacing="0" role="presentation"
+    style="width:544px;height:182px;border-collapse:collapse;background-color:#ffffff;border-radius:12px;box-shadow:0 4px 14px rgba(0,0,0,0.08);font-family:'Cabin', Arial, Helvetica, sans-serif;color:#333333;">
     <tr>
         <td style="padding:24px 28px 0 28px;">
-            <table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;">
+            <table cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;">
                 <tr>
-                    <td style="text-align:left;">
+                    <td style="text-align:left;width:371px;height:182px;">
                         <img src="{{ $logo }}" alt="Greenex" style="max-width:180px;height:auto;">
                         <div style="font-size:20px;font-weight:700;color:#389542;text-transform:uppercase;">
                             {{ $name }}
@@ -51,90 +51,93 @@
                             {{ $role }}
                         </div>
                         <div>
-                            <table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;">
-                <tr>
-                    <td style="width:30%;vertical-align:top;padding-right:12px;">
-                        <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;">
-                            <tr>
-                                <td style="padding:6px 0;">
-                                    <table role="presentation" cellpadding="0" cellspacing="0"
-                                        style="border-collapse:collapse;" style="width:30%;">
-                                        <tr>
-                                            <td
-                                                style="padding-right:10px;width:18px;vertical-align:top;margin-left:5px;">
-                                                <img src="{{ $iconMail }}" alt="Correo"
-                                                    style="width:22px;height:18px;display:block;">
-                                            </td>
-                                            <td style="font-size:12px;">
-                                                <a href="mailto:{{ $email }}"
-                                                    style="color:#333333;text-decoration:none;">{{ $email }}</a>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </td>
-
-                                @if ($addressHtml)
-
-                                    <td style="">
-                                        <table role="presentation" cellpadding="0" cellspacing="0"
-                                            style="border-collapse:collapse;">
-                                            <tr>
-                                                <td
-                                                    style="padding-left:10px;padding-right:10px;width:18px;vertical-align:middle;">
-                                                    <img src="{{ $iconLocation }}" alt="Ubicación"
-                                                        style="width:18px;height:18px;display:block;">
-                                                </td>
-                                                <td style="font-size:12px;color:#555555;">
-                                                    {!! $addressHtml !!}
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </td>
-                                @endif
-
-
-
-                            </tr>
-
-                            @if ($phone)
+                            <table width="100%" cellpadding="0" cellspacing="0" role="presentation"
+                                style="border-collapse:collapse;">
                                 <tr>
-                                    <td style="padding:0px 0;">
+                                    <td style="width:30%;vertical-align:top;padding-right:12px;">
                                         <table role="presentation" cellpadding="0" cellspacing="0"
                                             style="border-collapse:collapse;">
                                             <tr>
-                                                <td style="padding-right:10px;width:18px;vertical-align:top;">
-                                                    <img src="{{ $iconPhone }}" alt="Teléfono"
-                                                        style="width:18px;height:22px;display:block;">
+                                                <td style="padding:6px 0;">
+                                                    <table role="presentation" cellpadding="0" cellspacing="0"
+                                                        style="border-collapse:collapse;" style="width:30%;">
+                                                        <tr>
+                                                            <td
+                                                                style="padding-right:10px;width:18px;vertical-align:top;margin-left:5px;">
+                                                                <img src="{{ $iconMail }}" alt="Correo"
+                                                                    style="width:22px;height:18px;display:block;">
+                                                            </td>
+                                                            <td style="font-size:12px;">
+                                                                <a href="mailto:{{ $email }}"
+                                                                    style="color:#333333;text-decoration:none;">{{ $email }}</a>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
                                                 </td>
-                                                <td style="font-size:12px;">
-                                                    <a href="{{ $whatsUrl ?: 'tel:' . $whatsPhone }}"
-                                                        style="color:#333333;text-decoration:none;">+{{ $phone }}</a>
-                                                </td>
+
+                                                @if ($addressHtml)
+                                                    <td style="">
+                                                        <table role="presentation" cellpadding="0" cellspacing="0"
+                                                            style="border-collapse:collapse;">
+                                                            <tr>
+                                                                <td
+                                                                    style="padding-left:10px;padding-right:10px;width:18px;vertical-align:middle;">
+                                                                    <img src="{{ $iconLocation }}" alt="Ubicación"
+                                                                        style="width:18px;height:18px;display:block;">
+                                                                </td>
+                                                                <td style="font-size:12px;color:#555555;">
+                                                                    {!! $addressHtml !!}
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                @endif
+
+
+
                                             </tr>
+
+                                            @if ($phone)
+                                                <tr>
+                                                    <td style="padding:0px 0;">
+                                                        <table role="presentation" cellpadding="0" cellspacing="0"
+                                                            style="border-collapse:collapse;">
+                                                            <tr>
+                                                                <td
+                                                                    style="padding-right:10px;width:18px;vertical-align:top;">
+                                                                    <img src="{{ $iconPhone }}" alt="Teléfono"
+                                                                        style="width:18px;height:22px;display:block;">
+                                                                </td>
+                                                                <td style="font-size:12px;">
+                                                                    <a href="{{ $whatsUrl ?: 'tel:' . $whatsPhone }}"
+                                                                        style="color:#333333;text-decoration:none;">+{{ $phone }}</a>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            @endif
                                         </table>
                                     </td>
-                                </tr>
-                            @endif
-                        </table>
-                    </td>
 
-            </table>
+                            </table>
                         </div>
 
 
                     </td>
-                    <td>
+                    <td style="width:173px;height:182px;">
                         @if ($qrSvg)
                             <div style="width:150px;height:150px;margin:0 auto;padding:10px;">
                                 {!! $qrSvg !!}
                             </div>
                         @elseif($qrImg)
-                             <div style="width:150px;height:150px;margin:0 auto;padding:10px;">
+                            <div style="width:150px;height:150px;margin:0 auto;padding:10px;">
                                 <img src="{{ $qrImg }}" alt="QR de contacto"
                                     style="width:100%;height:100%;display:block;">
                             </div>
                         @else
-                            <div style="width:150px;height:150px;margin:0 auto;padding:10px;border:#389542 solid 1px;border-radius:12px">
+                            <div
+                                style="width:150px;height:150px;margin:0 auto;padding:10px;border:#389542 solid 1px;border-radius:12px">
                                 QR no disponible
                             </div>
                         @endif
@@ -143,7 +146,8 @@
                                 <tr>
                                     @if ($linkedin)
                                         <td style="padding:0 6px;">
-                                            <a href="{{ $linkedin }}" target="_blank" style="display:inline-block;">
+                                            <a href="{{ $linkedin }}" target="_blank"
+                                                style="display:inline-block;">
                                                 <img src="{{ $iconLinkedin }}" alt="LinkedIn" width="20"
                                                     height="20" style="display:block;">
                                             </a>
@@ -151,7 +155,8 @@
                                     @endif
                                     @if ($instagram)
                                         <td style="padding:0 6px;">
-                                            <a href="{{ $instagram }}" target="_blank" style="display:inline-block;">
+                                            <a href="{{ $instagram }}" target="_blank"
+                                                style="display:inline-block;">
                                                 <img src="{{ $iconInstagram }}" alt="Instagram" width="20"
                                                     height="20" style="display:block;">
                                             </a>
@@ -159,7 +164,8 @@
                                     @endif
                                     @if ($website)
                                         <td style="padding:0 6px;">
-                                            <a href="{{ $website }}" target="_blank" style="display:inline-block;">
+                                            <a href="{{ $website }}" target="_blank"
+                                                style="display:inline-block;">
                                                 <img src="{{ $iconWeb }}" alt="Sitio web" width="20"
                                                     height="20" style="display:block;">
                                             </a>
