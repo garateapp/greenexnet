f5aff109b76e397aeb4f555689631bf46c65c3ab
calgreenex <carlos.alvarez@greenex.cl>
2026-02-04 15:20:30 -0300

    .

--- app/Http/Controllers/Api/V1/Admin/KpturaSyncController.php
+++ app/Http/Controllers/Api/V1/Admin/KpturaSyncController.php
@@ -10,6 +10,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
 
 class KpturaSyncController extends Controller
 {
