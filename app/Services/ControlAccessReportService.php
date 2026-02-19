f5c8f391b4eeac9994bcfd22fb4e9f3e6404e18b
calgreenex <carlos.alvarez@greenex.cl>
2026-02-19 09:16:19 -0300

    .

--- app/Services/ControlAccessReportService.php
+++ app/Services/ControlAccessReportService.php
@@ -18,7 +18,42 @@ class ControlAccessReportService
             ->select('personal_id', DB::raw('COUNT(*) as attendance_count'))
             ->whereBetween('timestamp', [$start, $end])
             ->groupBy('personal_id');
-
+        Log::info('attendanceSub: ' . $attendanceSub->toSql());
+        Log::info("ControlAccessLog: " . ControlAccessLog::from('control_access_logs as c')
+            ->join('personals as p', 'c.personal_id', '=', 'p.codigo')
+            ->join('entidads as e', 'e.id', '=', 'p.entidad_id')
+            ->leftJoinSub($attendanceSub, 'a', function ($join) {
+                $join->on('a.personal_id', '=', 'p.id');
+            })
+            ->select([
+                DB::raw('DATE(c.fecha) as fecha'),
+                'p.codigo as personal_id',
+                'p.rut',
+                'p.nombre',
+                'e.nombre as departamento',
+                'e.id as entidad_id',
+                DB::raw('MIN(c.primera_entrada) as primera_marca'),
+                DB::raw('MAX(c.ultima_salida) as ultima_salida'),
+                'p.id as personal_db_id',
+                DB::raw('COALESCE(a.attendance_count, 0) as attendance_count'),
+                DB::raw('CASE WHEN a.attendance_count IS NULL THEN 1 ELSE 0 END as sin_asistencia'),
+            ])
+            ->whereBetween('c.fecha', [$start, $end])
+            ->whereIn('p.entidad_id', $entidades)
+            ->whereNull('p.deleted_at')
+            ->groupBy(
+                DB::raw('DATE(c.fecha)'),
+                'p.id',
+                'p.codigo',
+                'p.rut',
+                'p.nombre',
+                'e.nombre',
+                'e.id',
+                'a.attendance_count'
+            )
+            ->orderBy('fecha')
+            ->orderBy('p.rut')
+            ->toSql());
         return ControlAccessLog::from('control_access_logs as c')
             ->join('personals as p', 'c.personal_id', '=', 'p.codigo')
             ->join('entidads as e', 'e.id', '=', 'p.entidad_id')
